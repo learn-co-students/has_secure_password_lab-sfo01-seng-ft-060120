6aa76429ae70ae5172245bc445b583ed5ccb404d
sfespinosa <scottjames.espinosa@gmail.com>
2020-07-01 10:59:55 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,5 @@ class SessionsController < ApplicationController
         @user = User.find_by(name: params[:user][:name])
         return head(:forbidden) unless @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to
     end
 end
